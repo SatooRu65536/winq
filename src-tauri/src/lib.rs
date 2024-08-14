5d573c0204305abccbe08945ef46eb5deb7c9d48
satooru65536 <ryuto4619@gmail.com>
2024-08-15 02:24:49 +0900

    feat: window サイズ

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ pub fn run(builder: Builder) {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(builder.invoke_handler())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
